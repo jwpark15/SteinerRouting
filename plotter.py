4860b9a1dd636cb499085926bf7e32ec68ac70cb
jpark832 <jpark832@gatech.edu>
2021-04-29 19:30:09 -0400

    updated for LRST. no longer needed filename to be hardcoded

--- plotter.py
+++ plotter.py
@@ -48,6 +48,56 @@ def plot_MST(file_pts, file_mst, ax):
     return ax
 
 
+
+def get_LRST_lines(filename, x, y):
+    parents = []
+    children = []
+    orientations = []
+    with open(filename, 'r') as f:
+        line = f.readline()
+        while line != '':
+            parents.append(int(line.split()[0]))
+            children.append(int(line.split()[1]))
+            orientations.append(int(line.split()[2]))
+
+            line = f.readline()
+
+    plot_lines = [] 
+    for i in range(1,len(parents)):
+        i1 = parents[i]
+        i2 = children[i]
+        if (orientations[i] == 0):
+            # lower L
+            min_y = min(y[i1], y[i2])
+            plot_lines.append([(min(x[i1], x[i2]), min_y), (max(x[i1], x[i2]), min_y)])
+            if (y[i1] > y[i2]):
+                plot_lines.append([(x[i1], y[i2]), (x[i1], y[i1])])
+            else:
+                plot_lines.append([(x[i2], y[i1]), (x[i2], y[i2])])
+        else:
+            # upper L
+            max_y = max(y[i1], y[i2])
+            plot_lines.append([(min(x[i1], x[i2]), max_y), (max(x[i1], x[i2]), max_y)])
+            if (y[i1] < y[i2]):
+                plot_lines.append([(x[i1], y[i2]), (x[i1], y[i1])])
+            else:
+                plot_lines.append([(x[i2], y[i1]), (x[i2], y[i2])])
+
+    return plot_lines
+
+
+def plot_LRST(file_pts, file_lrst, ax):
+    x, y = get_points(file_pts) 
+    lrst_lines = get_LRST_lines(file_lrst, x, y)    
+
+    # plot
+    lc = LineCollection(lrst_lines)
+    ax.scatter(x,y)
+    ax.add_collection(lc)
+    
+    return ax
+    
+
 def get_grid_size(file_pts):
     end_ind = file_pts.rfind('_')
     first_ind = file_pts[:end_ind].rfind('_')
@@ -55,7 +105,7 @@ def get_grid_size(file_pts):
     return grid_size
 
 # TODO add plots for L-RST and Kahng/Robins
-def generate_plots(file_pts, file_mst):
+def generate_plots(file_pts, file_mst, file_lrst):
     # initialize plots
     fig, ax = plt.subplots(1,3)
     grid_size = get_grid_size(file_pts)
@@ -66,7 +116,7 @@ def generate_plots(file_pts, file_mst):
     
     # MST
     ax[0] = plot_MST(file_pts, file_mst, ax[0])
-
+    ax[1] = plot_LRST(file_pts, file_lrst, ax[1])
 
     # L-RST
 
@@ -77,4 +127,8 @@ def generate_plots(file_pts, file_mst):
     return
 
 if __name__ == "__main__":
-    generate_plots("Points/points_10_5.pts", "primResults.txt")
+    with open("benchmark.txt", 'r') as f:
+        points_file = f.readline()
+
+    print(points_file)
+    generate_plots(points_file, "primResults.txt", "LRSTResults.txt")
